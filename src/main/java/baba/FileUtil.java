f4ebad2fc74106da7967a3f160ee96487561acbc
yogesh <sriniyogesh@gmail.com>
2017-06-07 00:06:15 +0530

    fileutil added

--- src/main/java/baba/FileUtil.java
+++ src/main/java/baba/FileUtil.java
@@ -2,8 +2,12 @@ package baba;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.IOException; 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream; 
 
 public class FileUtil {
 	
@@ -25,20 +29,120 @@ public class FileUtil {
 	
 	public static void move(String srcFile, String distFile){
 		
+		InputStream inStream = null;
+		OutputStream outStream = null;
+
+	    	try{
+
+	    	    File afile =new File(srcFile);
+	    	    File bfile =new File(distFile);
+
+	    	    inStream = new FileInputStream(afile);
+	    	    outStream = new FileOutputStream(bfile);
+
+	    	    byte[] buffer = new byte[1024];
+
+	    	    int length;
+	    	    //copy the file content in bytes
+	    	    while ((length = inStream.read(buffer)) > 0){
+
+	    	    	outStream.write(buffer, 0, length);
+
+	    	    }
+
+	    	    inStream.close();
+	    	    outStream.close();
+
+	    	    //delete the original file
+	    	    afile.delete();
+
+	    	    System.out.println("File is moved successful!");
+
+	    	}catch(IOException e){
+	    	    e.printStackTrace();
+	    	}
 	}
 	
 	public static void copy(String srcFile, String distFile){
-		
+		InputStream inStream = null;
+		OutputStream outStream = null;
+
+	    	try{
+
+	    	    File afile =new File(srcFile);
+	    	    File bfile =new File(distFile);
+
+	    	    inStream = new FileInputStream(afile);
+	    	    outStream = new FileOutputStream(bfile);
+
+	    	    byte[] buffer = new byte[1024];
+
+	    	    int length;
+	    	    //copy the file content in bytes
+	    	    while ((length = inStream.read(buffer)) > 0){
+
+	    	    	outStream.write(buffer, 0, length);
+
+	    	    }
+
+	    	    inStream.close();
+	    	    outStream.close();
+
+	    	    System.out.println("File is copied successful!");
+
+	    	}catch(IOException e){
+	    	    e.printStackTrace();
+	    	}		
 	}
 	
 	public static void moveDir(String srcFile, String distFile){
-		
+		File srcFile2 = new File(srcFile);
+		File distFile2 = new File(appendPath(distFile,srcFile.substring(srcFile.lastIndexOf('/')+1,srcFile.length())));
+		if(srcFile2.exists()){
+			if(srcFile2.isDirectory()){
+				if(!distFile2.exists()){
+					distFile2.mkdirs();
+				}			
+				for(String subPath: srcFile2.list()){
+					moveDir(appendPath(srcFile,subPath), distFile2.getAbsolutePath());
+				}
+			}else{
+				move(srcFile, distFile2.getAbsolutePath());
+			}
+			srcFile2.delete();
+		}		
 	}
 	
 	public static void copyDir(String srcFile, String distFile){
+		File srcFile2 = new File(srcFile);
+		File distFile2 = new File(appendPath(distFile,srcFile.substring(srcFile.lastIndexOf('/')+1,srcFile.length())));
+		if(srcFile2.exists()){
+			if(srcFile2.isDirectory()){
+				if(!distFile2.exists()){
+					distFile2.mkdirs();
+				}			
+				for(String subPath: srcFile2.list()){
+					copyDir(appendPath(srcFile,subPath), distFile2.getAbsolutePath());
+				}
+			}else{
+				copy(srcFile, distFile2.getAbsolutePath());
+			}
+		}
 		
 	}
 	
+	public static String appendPath(String...paths){
+		StringBuilder fullPath = new StringBuilder();
+		for(String path : paths ){
+			fullPath.append(path).append("/");
+		}
+		return fullPath.substring(0, fullPath.length()-1);
+		
+	}
+	
+	public static void main(String args[]){
+		moveDir("D:/tmp/hadoop-Ace", "D:/tmp/baba");
+	}
 	
 
 }
